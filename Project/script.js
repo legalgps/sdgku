c6a71b5fc63ebc94fb50b0470ed54b863fbf6631
legalgps <38673628+legalgps@users.noreply.github.com>
2021-07-07 21:44:36 -0500

    104_Assignments 1&3

---
+++ Project/script.js
@@ -0,0 +1,45 @@
+var task1={
+    namd:"Do my HW",
+    priority:"high",
+    day:"Monday"
+}
+
+console.log(task1.name);
+console.log(task1[`day`]);
+
+var task2={
+    name:"Play Zelda",
+    priority:"Low",
+    day:"Saturday"
+}
+
+console.log(task2);
+
+var task3={
+    name: "Go to a restaurant",
+    priority: "Medium",
+    day:"Today"
+}
+
+console.log(task3[`day`]);
+
+function.display(task)[
+    console.log(task.day);
+    document.writeln(task.name+"<br>");    
+]
+
+display(task1);
+display(task2);
+
+var student1={
+    name:"Chris",
+    email:chris@legalgps.com,
+    password:"pw123",
+    courses:["101","102","103"],
+    address"{
+        street:"Atalanta",
+        number:"307"
+    },
+    points:[95,45,23,78]
+}
+console.log(student1.name,student1.email,student1.points[0])
